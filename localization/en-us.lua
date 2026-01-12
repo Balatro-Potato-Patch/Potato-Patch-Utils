753ce74ca10e2a073bc307fa39043d22c3398cf5
the-Astra <djs8743@rit.edu>
2026-01-11 23:32:22 -0500

    Initial commit

---
+++ localization/en-us.lua
@@ -0,0 +1,14 @@
+return {
+    misc = {
+        dictionary = {
+            -- Credits
+            ppu_art_credit = 'Art by ',
+            ppu_code_credit = 'Code by ',
+            ppu_and_spacer = ' and ',
+            ppu_comma_spacer = ', ',
+
+            -- UI
+            ppu_first_time_disable = "Do not show again",
+        }
+    }
+}
---
+++ main.lua
@@ -0,0 +1,44 @@
+PotatoPatchUtils = SMODS.current_mod
+
+--#region File Loading
+local nativefs = NFS
+
+local path_len = string.len(PotatoPatchUtils.path) + 1
+
+local function load_file_native(path)
+    if not path or path == "" then
+        error("No path was provided to load.")
+    end
+    local file_path = path
+    local file_content, err = NFS.read(file_path)
+    if not file_content then
+        return nil,
+        "Error reading file '" .. path .. "' for mod with ID '" .. PotatoPatchUtils.id .. "': " .. err
+    end
+    local short_path = string.sub(path, path_len, path:len())
+    local chunk, err = load(file_content, "=[SMODS " .. PotatoPatchUtils.id .. ' "' .. short_path .. '"]')
+    if not chunk then
+        return nil,
+        "Error processing file '" .. path .. "' for mod with ID '" .. PotatoPatchUtils.id .. "': " .. err
+    end
+    return chunk
+end
+local function load_files(path)
+    local info = nativefs.getDirectoryItemsInfo(path)
+    table.sort(info, function(a, b)
+        return a.name < b.name
+    end)
+    for _, v in ipairs(info) do
+        if string.find(v.name, ".lua") and not blacklist[v.name] then -- no X.lua.txt files or whatever unless they are also lua files
+            local f, err = load_file_native(path .. "/" .. v.name)
+            if f then
+                f()
+            else
+                error("error in file " .. v.name .. ": " .. err)
+            end
+        end
+    end
+end
+local path = PotatoPatchUtils.path .. '/src'
+load_files(path)
+--#endregion
